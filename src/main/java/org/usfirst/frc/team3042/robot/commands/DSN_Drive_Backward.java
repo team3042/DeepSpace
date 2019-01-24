6ec54b0857ef6b30d25d1287d4e7d1c7798e8ace
Isopod00 <anthonybrog@gmail.com>
2019-01-23 20:15:56 -0600

    DSN_Drive
    Adds the DSN_Drive subsystem and the DSN_Drive_Forward and DSN_Drive_Backward commands

---
+++ src/main/java/org/usfirst/frc/team3042/robot/commands/DSN_Drive_Backward.java
@@ -0,0 +1,72 @@
+package org.usfirst.frc.team3042.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import org.usfirst.frc.team3042.lib.Log;
+import org.usfirst.frc.team3042.robot.Robot;
+import org.usfirst.frc.team3042.robot.RobotMap;
+import org.usfirst.frc.team3042.robot.subsystems.DSN_Drive;
+
+
+/** ExampleCommand ************************************************************
+ * A template for commands.
+ */
+public class DSN_Drive_Backward extends Command {
+	/** Configuration Constants ***********************************************/
+	private static final Log.Level LOG_LEVEL = RobotMap.LOG_EXAMPLE_SUBSYSTEM;
+	
+	
+	/** Instance Variables ****************************************************/
+	Log log = new Log(LOG_LEVEL, getName());
+	DSN_Drive DSN_Drive = Robot.DSN_Drive;
+	
+	
+	/** ExampleCommand ********************************************************
+	 * Required subsystems will cancel commands when this command is run.
+	 */
+	public DSN_Drive_Backward() {
+		log.add("Constructor", Log.Level.TRACE);
+		
+		requires(DSN_Drive);
+	}
+
+	
+	/** initialize ************************************************************
+	 * Called just before this Command runs the first time
+	 */
+	protected void initialize() {
+		log.add("Initialize", Log.Level.TRACE);
+	}
+
+	
+	/** execute ***************************************************************
+	 * Called repeatedly when this Command is scheduled to run
+	 */
+	protected void execute() {
+	}
+	
+	
+	/** isFinished ************************************************************	
+	 * Make this return true when this Command no longer needs to run execute()
+	 */
+	protected boolean isFinished() {
+		return false;
+	}
+
+	
+	/** end *******************************************************************
+	 * Called once after isFinished returns true
+	 */
+	protected void end() {
+		log.add("End", Log.Level.TRACE);
+	}
+
+	
+	/** interrupted ***********************************************************
+	 * Called when another command which requires one or more of the same
+	 * subsystems is scheduled to run
+	 */
+	protected void interrupted() {
+		log.add("Interrupted", Log.Level.TRACE);
+	}
+}
---
+++ src/main/java/org/usfirst/frc/team3042/robot/subsystems/DSN_Drive.java
@@ -0,0 +1,36 @@
+package org.usfirst.frc.team3042.robot.subsystems;
+
+import org.usfirst.frc.team3042.lib.Log;
+import org.usfirst.frc.team3042.robot.RobotMap;
+import org.usfirst.frc.team3042.robot.commands.DSN_Drive_Forward;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+
+/** DSN_Drive **********************************************************
+ * A subsystem for driving the DSN wheels
+ */
+public class DSN_Drive extends Subsystem {
+	/** Configuration Constants ***********************************************/
+	private static final Log.Level LOG_LEVEL = RobotMap.LOG_EXAMPLE_SUBSYSTEM;
+
+	
+	/** Instance Variables ****************************************************/
+	Log log = new Log(LOG_LEVEL, getName());
+	
+	
+	/** ExampleSubsystem ******************************************************
+	 * 
+	 */
+	public DSN_Drive() {
+		log.add("Constructor", LOG_LEVEL);
+	}
+	
+	
+	/** initDefaultCommand ****************************************************
+	 * Set the default command for the subsystem.
+	 */
+	public void initDefaultCommand() {
+		setDefaultCommand(new DSN_Drive_Forward());
+	}
+}

99f198a19d7419a3c7ac5a2b7763840aac1f64bb
Isopod00 <anthonybrog@gmail.com>
2019-01-28 12:35:06 -0600

    DSN_Drive Fixes
    Added some fixes to DSN_Drive including seperate forward and backward commands based on cargo roller commands. (Also deleted Elevator_Panel_Up command because I forgot to delete it yesterday)

--- src/main/java/org/usfirst/frc/team3042/robot/subsystems/DSN_Drive.java
+++ src/main/java/org/usfirst/frc/team3042/robot/subsystems/DSN_Drive.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team3042.robot.subsystems;
 
 import org.usfirst.frc.team3042.lib.Log;
 import org.usfirst.frc.team3042.robot.RobotMap;
-import org.usfirst.frc.team3042.robot.commands.DSN_Drive_Drive;
+import org.usfirst.frc.team3042.robot.commands.DSN_Drive_Forward;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
@@ -17,6 +17,9 @@ public class DSN_Drive extends Subsystem {
 	/** Configuration Constants ***********************************************/
 	private static final Log.Level LOG_LEVEL = RobotMap.LOG_DSN_DRIVE;
 	private static final int CAN_DSN = RobotMap.CAN_DSN;
+	private static final double FORWARDPOWER = RobotMap.FORWARDPOWER;
+	private static final double BACKWARDPOWER = RobotMap.BACKWARDPOWER;
+
 
 	
 	/** Instance Variables ****************************************************/
@@ -33,20 +36,30 @@ public class DSN_Drive extends Subsystem {
 
 	/** Command Control *******************************************************/
 	public void setPower(double power) {
-		power = Math.min(1.0, power);
-		power = Math.max(-1.0, power);
-
+		power = safetyCheck(power);
 		motor.set(ControlMode.PercentOutput, power);
 	}
 	public void stop() {
 		setPower(0.0);
 	}
-	
+	private double safetyCheck(double power) {
+		power = Math.min(1.0, power);
+		power = Math.max(-1.0, power);
+		return power;
+	}
 	
 	/** initDefaultCommand ****************************************************
 	 * Set the default command for the subsystem.
 	 */
 	public void initDefaultCommand() {
-		setDefaultCommand(new DSN_Drive_Drive());
+		setDefaultCommand(new DSN_Drive_Forward());
+	}
+
+	public void forward() {
+    	setPower(FORWARDPOWER);
+	}
+	
+	public void backward() {
+    	setPower(-1.0*BACKWARDPOWER);
 	}
 }
